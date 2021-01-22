cfacfb84b20a691125dbdb22ac3d9cb5190715d4
Silap Aliyev <3767237+s9v@users.noreply.github.com>
2021-01-23 00:11:24 +0900

    Add incomplete Dinic's algorithm implementation in Rust

---
+++ algorithms/dinic.rs
@@ -0,0 +1,42 @@
+struct Dinic {
+  // ?
+}
+
+impl Dinic {
+  pub fn new() -> Dinic {
+    Dinic {
+      // ?
+    }
+  }
+
+  pub fn max_flow() {
+    
+  }
+  
+  pub fn add_edge() {
+    
+  }
+
+  fn bfs() {
+    
+  }
+
+  fn dfs() {
+    
+  }
+}
+
+fn main() {
+  let dinic = Dinic::new();
+  
+  // let a = Vec::new();
+  let mut a = vec![4, 3, 2, 1];
+
+  a.push(5);
+
+  for i in 0..a.len() {
+    println!("{}> {}", i, a[i]);
+  }
+
+}
+
