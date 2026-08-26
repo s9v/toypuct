98df231c0581020a8d0c77c8fbfbe6ca767f2a01
Silap Aliyev <3767237+s9v@users.noreply.github.com>
2026-08-26 14:42:32 +0900

    add bin search solution

--- USACO/Contests/2026-1st-bronze/chip-exchange/solution.cpp
+++ USACO/Contests/2026-1st-bronze/chip-exchange/solution.cpp
@@ -1,11 +1,45 @@
 #include <iostream>
 using namespace std;
 
+#define dbg(X) cerr << #X << ": " << X << "\n"
+
 void solve() {
-    int a, b, ca, cb, fa;
+    long long a, b, ca, cb, fa;
     cin >> a >> b >> ca >> cb >> fa;
-    int x = max((max(fa - a, 0) + ca - 1) / ca * 1LL * cb - b, 0LL);
-    cout << x << "\n";
+
+    long long lo = 0;
+    long long hi = 1LL << 60;
+    long long ans = 1LL << 60;
+    while (lo <= hi) {
+        long long mid = (lo + hi) / 2;
+
+        long long min_a = min(
+            a + mid + b / cb * ca,
+            a + (b + mid) / cb * ca
+        );
+        if ((b + mid) % cb + 1 < ca) {
+            min_a = min(
+                min_a,
+                a + (b + mid) / cb * ca + ((b + mid) % cb + 1 - ca)
+            );
+        }
+
+        if (min_a >= fa) {
+            // dbg(mid);
+            // dbg(a + mid + b / cb * ca);
+            // dbg(a + (b + mid) / cb * ca);
+            // dbg(a + (b + mid) / cb * ca + ((b + mid) % cb + 1 - ca));
+            // dbg(min_a);
+            // cerr << "\n";
+
+            ans = min(ans, mid);
+            hi = mid - 1;
+        } else {
+            lo = mid + 1;            
+        }
+    }
+
+    cout << ans << "\n";
 }
 
 int main() {
@@ -18,3 +52,4 @@ int main() {
 
     return 0;
 }
+
