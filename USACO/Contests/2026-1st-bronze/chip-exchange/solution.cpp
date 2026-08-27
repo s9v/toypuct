176351ac15a1c049c445e38f0ee0a5c76b45e4fc
Silap Aliyev <3767237+s9v@users.noreply.github.com>
2026-08-27 15:20:01 +0900

    submit: 2/12

--- USACO/Contests/2026-1st-bronze/chip-exchange/solution.cpp
+++ USACO/Contests/2026-1st-bronze/chip-exchange/solution.cpp
@@ -8,28 +8,39 @@ void solve() {
     cin >> a >> b >> ca >> cb >> fa;
 
     long long lo = 0;
-    long long hi = 1LL << 60;
-    long long ans = 1LL << 60;
+    long long hi = 1LL << 30;
+    long long ans = 1LL << 30;
     while (lo <= hi) {
         long long mid = (lo + hi) / 2;
 
         long long min_a = min(
-            a + (b + mid) / cb * ca - max(ca - (b + mid) % cb - 1, 0LL),
-            (a + mid) + b / cb * ca - max(cb - b % cb - 1, 0LL)
+            a + max((b + mid) / cb * ca - max(ca - (b + mid) % cb - 1, 0LL), 0LL),
+            max((a + mid) - max(cb - b % cb - 1, 0LL), 0LL) + b / cb * ca
         );
 
+        // dbg(lo);
+        // dbg(hi);
+        // dbg(mid);
+
+        // dbg(a + (b + mid) / cb * ca - max(ca - (b + mid) % cb - 1, 0LL));
+        // dbg((a + mid) + b / cb * ca - max(cb - b % cb - 1, 0LL));
+        // dbg(min_a);
+
         if (min_a >= fa) {
-            // dbg(mid);
             // dbg(a + mid + b / cb * ca);
             // dbg(a + (b + mid) / cb * ca);
             // dbg(a + (b + mid) / cb * ca + ((b + mid) % cb + 1 - ca));
-            // dbg(min_a);
-            // cerr << "\n";
             ans = min(ans, mid);
             hi = mid - 1;
         } else {
             lo = mid + 1;            
         }
+
+        // int new_lo = lo;
+        // int new_hi = lo;
+        // dbg(new_lo);
+        // dbg(new_hi);
+        // cerr << "\n";
     }
 
     cout << ans << "\n";
